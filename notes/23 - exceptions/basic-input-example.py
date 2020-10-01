f64b2c6dafc16037870bcaaba4117dfd795a6f55
Chris Brooks-Prenger <brooksprench@hdsb.ca>
2020-10-01 11:07:10 -0400

    Add extra exception example

---
+++ notes/23 - exceptions/basic-input-example.py
@@ -0,0 +1,22 @@
+#-----------------------------------------------------------------------------
+# Name:        Catching Exceptions (try-except.py)
+# Purpose:     To provide example of a simple input loop using try-catch
+#
+# Author:      Mr. Brooks
+# Created:     01-Oct-2020
+# Updated:     01-Oct-2020
+#-----------------------------------------------------------------------------
+
+while True: #Start an infinite loop
+    
+    value = input('Enter a number between -100 and 100: ') #Get a value from the user
+    
+    try:
+        value = int(value)   #Convert the value to an int
+        
+    except Exception as e:
+        print('Something went wrong: ' + str(e)) #You should probably add a nicer error message
+
+    else:
+        #No exception was thrown, so break out of the infinite loop
+        break
