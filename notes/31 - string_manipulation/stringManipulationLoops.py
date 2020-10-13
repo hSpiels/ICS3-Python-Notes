865a0165f15a2e011c1adeb6fd550d7e70f11a56
Chris Brooks-Prenger <brooksprench@hdsb.ca>
2020-10-13 10:23:32 -0400

    Add Replace Letter Function

--- notes/31 - string_manipulation/stringManipulationLoops.py
+++ notes/31 - string_manipulation/stringManipulationLoops.py
@@ -15,3 +15,11 @@ print(blanks)
 
 for i in range( len(word)):
     print(i, word[i], blanks[i])
+    if word[i] == userinput:
+        blanks[i]
+    
+def replaceLetter(stringIn, index, letter):
+    return stringIn[:index] + letter + stringIn[index + 1:]
+
+print (replaceLetter(word, 3, 'z'))
+
